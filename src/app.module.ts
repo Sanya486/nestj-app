25c593dad96372be80fad3119cd02599edce2e51
Sanya486 <sanyok486@gmail.com>
2024-01-05 15:32:16 +0200

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,10 @@ import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import 'dotenv/config';
+
 @Module({
-  imports: [
-    TodosModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://sanyok486:JicCP3SuhrLTSRig@cluster0.nd1vspl.mongodb.net/todos_DB',
-    ),
-  ],
+  imports: [TodosModule, MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
