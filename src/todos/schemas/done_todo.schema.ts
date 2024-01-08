94e20eace5d79cb35ac2e11e423850a3d757cb7e
Sanya486 <sanyok486@gmail.com>
2024-01-08 13:48:06 +0200

    add validation pipe

--- src/todos/schemas/done_todo.schema.ts
+++ src/todos/schemas/done_todo.schema.ts
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { HydratedDocument } from 'mongoose';
 
 export type DoneTodoDocument = HydratedDocument<DoneTodo>;
 
 @Schema()
 export class DoneTodo {
+  @IsString()
+  @IsNotEmpty()
   @Prop()
   title: string;
 }
--- src/todos/schemas/undone_todo.schema.ts
+++ src/todos/schemas/undone_todo.schema.ts
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export type UndoneTodoDocument = HydratedDocument<UndoneTodo>;
 
 @Schema()
 export class UndoneTodo {
+  @IsString()
+  @IsNotEmpty()
   @Prop()
   title: string;
 }
